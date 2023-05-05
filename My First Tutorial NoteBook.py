3b7d8662ce44ee7dd16e54865be444d3a8427ddc
daddypurl <129981462+daddypurl@users.noreply.github.com>
2023-05-05 22:48:29 +0100

    Add files via upload

---
+++ My First Tutorial NoteBook.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+print('My First Notebook!')
+
+
+# # Assignment:
+# 
+# *Create a Jupyter Notebook
+# *Insert code and markdown cells
+# *Share your notebook through GitHub
+# 
+# In this cell I have demonstrated the use of Markdown and below this cell, I'll be demonstrating the use of Code
+
+# # Lists (Arrays)
+
+# In[2]:
+
+
+friends = ["Monica" , "Cyndi" , "Kelvin" , "Sammy" , "Joe"]
+print(friends[3])
+
+
+# ## variable
+
+# In[3]:
+
+
+age = 40
+print (age)
+
